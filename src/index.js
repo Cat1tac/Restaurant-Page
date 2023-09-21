a6b9a916697ea538b4df0183e413e7d8cbeff61c
Cat1tac <waynepowell235@gmail.com>
2023-09-21 02:43:41 -0400

    Images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './style.css';
-import './mcDonalds.jpeg';
-import './orangeCake.jpeg';
-import './oranges.jpg';
-import './owner.jpg';
 import {mainPage} from './home';
 import menuPage from './menu';
 import contactPage from './contact';
